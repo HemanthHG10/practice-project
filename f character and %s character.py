99ca766c64522b21d7ec80d89ba5089929fb80b1
Hemanth HG <hemanthhg240@gmail.com>
2025-05-19 19:01:52 +0530

    Delete f character and %s character.py

--- f character and %s character.py
+++
@@ -1,9 +0,0 @@
-name=str(input('enter your name:'))
-age=int(input('enter your current age'))
-print('Hello my name is %s.  i am %s years old.'%(name,age))
-
-
-
-name=str(input('enter your name:'))
-age=int(input('enter your current age'))
-print(f'Hello my name is {name}.  i am {age} years old.')
