680f097fee75015ddbc920646e963df961231959
Hemanth HG <hemanthhg240@gmail.com>
2025-05-19 19:05:52 +0530

    Delete vs guess the number.py

--- vs guess the number.py
+++
@@ -1,9 +0,0 @@
-import random
-n=input('int(enter the value  between 1 and 100)')
-choices=(1,100)
-user_guess=input('int(enter the number between 1 and 100)')
-random.randint(1,100)
-if user_guess==choices:
-                       print()
-
-
