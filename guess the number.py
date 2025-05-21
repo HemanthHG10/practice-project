ba7673d01398b225e3d9d85a7ba374d4e50d5618
Hemanth HG <hemanthhg240@gmail.com>
2025-05-21 19:07:28 +0530

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,7 @@
+import random
+number=random.randint(1,100)
+guess=int(input("guess the number between 1 and 100:"))
+if guess==number:
+    print("you guessed it!!")
+else:
+    print(f"wrong! the number was {number}")
