64a54c620f5acc6cbdd0ce886859d1ca622e2652
Hemanth HG <hemanthhg240@gmail.com>
2025-05-21 18:48:50 +0530

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,12 @@
+def run_zigzag(num_asterisks):
+    indentation=0
+    direction=1 # 1 for increasing,-1 for decreasimg
+
+    while num_asterisks > 0:
+        print(" "*indentation + "*")
+        indentation+=direction
+        num_asterisks-=1
+
+        if indentation==0  or indentation==5:#adjust upper limit as needed
+            direction*=-1
+run_zigzag(50)
